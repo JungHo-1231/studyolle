a47cb65851626e443be00313c07760cb6a0621bb
jung <jungpark007@naver.com>
2021-03-28 19:01:38 +0900

    add basic spring security setting add sing-up view add controller sign-up handler add sing-up test code

---
+++ src/main/java/com/inflearn/studyolle/studyolle/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.inflearn.studyolle.studyolle.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login",
+                        "/sign-up", "/check-email",
+                        "/check-email-token", "/email-login",
+                        "/check-email-login", "/login-link")
+                    .permitAll()
+                // profile 은 get 만 허용
+                .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources()
+                .atCommonLocations());
+
+    }
+}
