7a9157950e308344019464a155f47033c6aa56e9
Jung Ho <jungho1231@flexis.co.kr>
2021-04-05 19:11:09 +0900

    new commit message

--- src/main/java/com/inflearn/studyolle/studyolle/account/AccountService.java
+++ src/main/java/com/inflearn/studyolle/studyolle/account/AccountService.java
@@ -8,11 +8,13 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -40,7 +42,7 @@ public class AccountService {
         newAccount.generateEmailCheckToken();
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setSubject("스터디올래, 회원 가입 인증");
-        mailMessage.setText("/check-email-token?token = " + newAccount.getEmailCheckToken()+
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()+
                 "&email=" + newAccount.getEmail()
         );
 
@@ -68,11 +70,13 @@ public class AccountService {
     // 정석대로 하기 위해서는 암호화 되지 않은 비밀번호가 필요하기 때문에
     public void login(Account account) {
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
-                account.getNickname(),
+                new UserAccount(account),
                 account.getPassword(),
                 List.of(new SimpleGrantedAuthority("ROLE_USER")));
 
         SecurityContext context = SecurityContextHolder.getContext();
         context.setAuthentication(token);
     }
+
+
 }
